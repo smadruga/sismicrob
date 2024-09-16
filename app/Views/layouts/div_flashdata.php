c3dd2001334b578d9e725f2db1a96ba93b1cd2f7
Rodrigo Campos <rpcrodrigo@gmail.com>
2024-09-16 12:30:25 -0300

    UPDATE:FLASHDATA: ALterada a forma como as divs de alerta são aparesentadas. Antes eram estáticas e incoporadas no código. Agora elas são mais curtas e flutuam sobre a tela.

--- app/Views/layouts/div_flashdata.php
+++ app/Views/layouts/div_flashdata.php
@@ -1,15 +1,15 @@
 <?php if(session()->getFlashdata('success')) { ?>
-    <div class="alert alert-success alert-dismissible" id="flashdata">
+    <div class="alert alert-success alert-dismissible position-fixed top-0 end-0 m-3" id="flashdata" style="z-index: 1050;">
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         <?php echo session()->getFlashdata('success') ?>
     </div>
 <?php } elseif(session()->getFlashdata('failed')) { ?>
-    <div class="alert alert-danger alert-dismissible" id="flashdata">
+    <div class="alert alert-danger alert-dismissible position-fixed top-0 end-0 m-3" id="flashdata" style="z-index: 1050;">
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         <?php echo session()->getFlashdata('failed') ?>
     </div>
 <?php } elseif(session()->getFlashdata('nochange')) { ?>
-    <div class="alert alert-warning alert-dismissible" id="flashdata">
+    <div class="alert alert-warning alert-dismissible position-fixed top-0 end-0 m-3" id="flashdata" style="z-index: 1050;">
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         <?php echo session()->getFlashdata('nochange') ?>
     </div>
